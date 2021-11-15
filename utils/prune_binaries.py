8d06c7a6a21b856b8caa5fb532680f9a936a7acc
Luka-Filipovic <filipovicluka3@gmail.com>
2021-11-15 21:17:55 +0100

    init

--- utils/prune_binaries.py
+++ utils/prune_binaries.py
@@ -10,6 +10,9 @@ import argparse
 from pathlib import Path
 
 from _common import ENCODING, get_logger, add_common_params
+import sys
+import os
+import stat
 
 
 def prune_dir(unpack_root, prune_files):
@@ -24,6 +27,11 @@ def prune_dir(unpack_root, prune_files):
         file_path = unpack_root / relative_file
         try:
             file_path.unlink()
+        # read-only files can't be deleted on Windows
+        # so remove the flag and try again.
+        except PermissionError:
+            os.chmod(file_path, stat.S_IWRITE)
+            file_path.unlink()
         except FileNotFoundError:
             unremovable_files.add(Path(relative_file).as_posix())
     return unremovable_files
@@ -32,7 +40,7 @@ def prune_dir(unpack_root, prune_files):
 def _callback(args):
     if not args.directory.exists():
         get_logger().error('Specified directory does not exist: %s', args.directory)
-        exit(1)
+        sys.exit(1)
     if not args.pruning_list.exists():
         get_logger().error('Could not find the pruning list: %s', args.pruning_list)
     prune_files = tuple(filter(len, args.pruning_list.read_text(encoding=ENCODING).splitlines()))
@@ -41,7 +49,7 @@ def _callback(args):
         get_logger().error('%d files could not be pruned.', len(unremovable_files))
         get_logger().debug('Files could not be pruned:\n%s',
                            '\n'.join(f for f in unremovable_files))
-        exit(1)
+        sys.exit(1)
 
 
 def main():
